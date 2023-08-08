43961ada7866c157f8449f1c9e405af14d5f046d
MostafaMohamed142 <simbawyy10@gmail.com>
2023-08-09 01:57:56 +0300

    adjustments

--- js/move.js
+++ js/move.js
@@ -1,43 +1,33 @@
-// let slider=["https://www.youtube.com/embed/zcI6SFiK_yk","https://www.youtube.com/embed/1ygQATx7_4g","https://www.youtube.com/embed/IN5TD4VRcSM","https://youtu.be/h9MSHl40z-g","https://youtu.be/Rx-4kSM743o"];
-// var thevideo="https://www.youtube.com/embed/zcI6SFiK_yk";
-// slider["0"]=thevideo;
+// Get the elements
+const sliderContainer = document.querySelector(".slider-container");
+const slider = sliderContainer.querySelector(".slider");
+const iframes = slider.querySelectorAll("iframe");
+const prevButton = sliderContainer.querySelector(".lft");
+const nextButton = sliderContainer.querySelector(".rght");
 
-// function rght(){for(i in slider){
-//     slider=document.getElementById("imgs").innerHTML = `
-//     <iframe src="${thevideo}" title="YouTube video player" frameborder="0"  allowfullscreen></iframe> 
-//     `;
-//     var i=slider["2"].lenth-1;
-//     console.log(slider);
+// Initialize current slide index
+let currentSlide = 0;
 
-//     i++;
-   
-// }};
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Show the iframe at the given index
+function showSlide(index) {
+  iframes.forEach((iframe, idx) => {
+    iframe.style.display = idx === index ? "block" : "none";
+  });
+  currentSlide = index;
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// Event listeners for navigation buttons
+prevButton.addEventListener("click", () => {
+  if (currentSlide > 0) {
+    showSlide(currentSlide - 1);
+  }
+});
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+nextButton.addEventListener("click", () => {
+  if (currentSlide < iframes.length - 1) {
+    showSlide(currentSlide + 1);
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+});
+
+// Initial slide
+showSlide(currentSlide);
